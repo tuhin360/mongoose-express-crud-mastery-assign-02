1950aba51e814ea06a1ab9d48bfaf546572d1b86
tuhin <tuhin153695@gmail.com>
2023-12-10 20:56:12 +0600

    initial project setup added

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,13 @@ const app: Application = express();
 
 //parsers
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
-  const a = 20;
-  res.send(a);
+  res.status(200).json({
+    status: 'success',
+    message: 'Welcome To Mongoose Express CRUD Mastery',
+  });
 });
 
 export default app;
