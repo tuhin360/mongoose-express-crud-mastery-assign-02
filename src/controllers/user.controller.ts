68bfd714fa50bc0bc5acc0b7a8728079dcd16d39
tuhin <tuhin153695@gmail.com>
2023-12-11 13:02:28 +0600

    user controller added

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import User from '../models/user.model';
+
+const createUser = async (req: Request, res: Response) => {
+  try {
+    const userData = req.body;
+    const result = await User.create(userData);
+    res.status(201).json({
+      status: 'success',
+      message: 'User created successfully',
+      data: result,
+    });
+  } catch (error: any) {
+    console.log(error);
+    res.status(500).json({
+      status: 'fail',
+      message: error.message || 'Something went wrong',
+    });
+  }
+};
+
+export const userController = {
+  createUser,
+};
